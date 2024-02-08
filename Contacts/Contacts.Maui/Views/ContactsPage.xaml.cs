920679981aa1145d1bf24c955e255d492caebd2b
trisha-07 <mehejabintrisha@gmail.com>
2024-02-08 23:14:33 +0100

    Fourth-Commit

--- Contacts/Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts/Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -6,15 +6,22 @@ public partial class ContactsPage : ContentPage
 	{
 		InitializeComponent();
 
-        List<string> contacts = new List<string>() {
-            "John Doe",
-            "Jane Doe",
-            "Tom Hanks",
-            "Frank Liu"};
+        List<Contact> contacts = new List<Contact>()
+
+         {
+            new Contact { Name="John Doe", Email="JohnDoe@gmail.com" },
+            new Contact { Name="Jane Doe", Email="JaneDoe@gmail.com"},
+            new Contact { Name="Tom Hanks", Email="TomHanks@gmail.com"},
+            new Contact { Name="Frank Liu", Email="Frankliu@gmail.com"},
+            };
 
         listContacts.ItemsSource = contacts;
+    }
 
-
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 	
     
--- Contacts/Contacts.Maui/obj/Debug/net8.0-android/Contacts.Maui.AssemblyInfo.cs
+++ Contacts/Contacts.Maui/obj/Debug/net8.0-android/Contacts.Maui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da5d2cb57df54913ddddfe24bd83af42e824640c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contacts.Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
