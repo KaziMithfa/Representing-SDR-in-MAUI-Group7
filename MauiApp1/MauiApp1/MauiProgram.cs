79d8b62d79008d4f97e4e7daae6f8f28e263394d
saleque143 <saleque.ahmed@stud.fra-uas.de>
2024-03-03 18:13:27 +0100

    Init For Main

--- MauiApp1/MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiApp1/MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-//Code Function
+
 namespace MauiApp1
 {
     public static class MauiProgram
@@ -22,7 +22,6 @@ namespace MauiApp1
 #endif
 
             return builder.Build();
-            Console.WriteLine("Check");
         }
     }
 }
