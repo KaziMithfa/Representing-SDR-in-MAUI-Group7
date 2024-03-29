fae7563ac6339e891fa527c191e2032740b3a73d
saleque143 <saleque.ahmed@stud.fra-uas.de>
2024-03-29 18:23:12 +0100

    Drawing functionality library addition and documents upload

---
+++ NeocortexApi/ISdrDrawerLib.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeocortexApi
+{
+    public interface ISdrDrawerLib
+    {
+        Task<string> GenerateSDRChartAsync(string csvContent, bool useFileInput, int highlightTouch, string figureName, string xAxisTitle,
+            string yAxisTitle, bool isHorizontal, int? maxCycles);
+    }
+}
